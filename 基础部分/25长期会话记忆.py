11dbb09fee804cec9202766936bc727dc26af053
chamoionak <2591558509@qq.com>
2026-05-09 22:44:10 +0800

    DAY6RAG项目+langchain基础+3

--- 基础部分/25长期会话记忆.py
+++ 基础部分/25长期会话记忆.py
@@ -84,7 +84,7 @@ def print_prompt(full_prompt):
 base_chain = prompt | print_prompt | model | str_parser
 
 def get_history(session_id):
-    return FileChatMessageHistory(session_id, "./chat_history")
+    return FileChatMessageHistory(session_id, "chat_history")
 
 # 创建一个新的链，对原有链增强功能：自动附加历史消息
 conversation_chain = RunnableWithMessageHistory(
